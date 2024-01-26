6636a410daada66c5b40222efd3a8a3039199767
Ivan Gonzalez <gonzalezgarzonivan@gmail.com>
2024-01-25 23:04:14 -0600

    adv work

--- global.R
+++ global.R
@@ -1,23 +1,49 @@
 library(dplyr)
-
-library(terra)
+#library(terra)
 library(sf)
 
 
-setwd('/srv/shiny-server/cuencas/')
-
-(load('bas_bind5307u1352_snip1724_nosnp1704_hort1879.RData')) # bas
-(load('qlSummary_qlList_1240stations.RData')) # "qlSummary" "qlList"
-#dim(qlSummary)
-#length(qlList)
+ # setwd('/srv/shiny-server/cuencas/')
+ # setwd('N:/Mi unidad/git/cuencas')
 
 # qlPts <- sf::read_sf('qlStationsn1404.shp')
 # dim(qlPts)
+# head(qlPts)
+# qlPts$newx <- as.numeric(do.call(rbind, strsplit(qlPts$geometry, ','))[, 1])
+# qlPts$newy <- as.numeric(do.call(rbind, strsplit(qlPts$geometry, ','))[, 2])
 # save(qlPts, file = 'qlPts_1404.RData')
-# table(bas$bastype)
 
-plot(qlPts)
-head(qlPts)
+(load('qlPts_1404.RData')) # qlPts
+# table(bas$bastype)
+qlPts$id2 <- gsub('xyz', '-', qlPts$qlid)
+
+
+(load('bas_bind5307u1352_snip1724_nosnp1704_hort1879.RData')) # bas
+(load('qlSummary_qlList_1240stations.RData')) # "qlSummary" "qlList"
+(load('newPoints_horton_990.RData')) # newPoints
+newPoints$qlid <- gsub('xyz', '-', newPoints$id)
+# dim(newPoints) # 990   
+# head(newPoints) # 990   
+
+
+# dim(qlPts) # 1404
+# 
+# head(qlPts)
+# head(bas@data)
+bas$qlid <- gsub('basin[0-9]_|basin[0-9][0-9]_', '', bas$full)
+#bas[, c('source', 'localid')] <- do.call(rbind, strsplit(bas$qlid, 'xyz'))[, 1]
+bas$source <- do.call(rbind, strsplit(bas$qlid, 'xyz'))[, 1]
+bas$localid <- do.call(rbind, strsplit(bas$qlid, 'xyz'))[, 2]
+bas$id2 <- gsub('xyz', '-', bas$qlid)
+
+class(bas)
+head(bas@data)
+
+# qlPts$havebas <- qlPts$qlid %in% bas$qlid
+# sum(qlPts$qlid %in% bas$qlid)
+
+# selBas <- bas[bas$qlid %in% qlPts$qlid, ]
+# dim(selBas)
 
 allzips <- readRDS("data/superzip.rds")
 allzips$latitude <- jitter(allzips$latitude)
@@ -42,7 +68,7 @@ cleantable <- allzips %>%
   )
 
 
-# system('sudo shiny; cd /home/shiny/connecting-landscapes; git add . ; git commit -m "Change coordiantes()"; git push')
+# sudo shiny; cd /home/shiny/connecting-landscapes; git add . ; git commit -m "Change coordiantes()"; git push
 # git pull main --rebase --autostash
 # sudo chown -R shiny:shiny .
 # git stash
